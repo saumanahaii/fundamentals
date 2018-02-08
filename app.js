12c779f82457197f271d71937b46380a7c89dc14
zach <me@zachwilliams.xyz>
2018-02-07 20:36:01 -0500

    add in hash support

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+//get the
+let hash = location.hash;
+if(hash.length>0){
+  //navigate to hash
+  console.log(location.hash.slice(1))
+  $("#frame").load(location.hash.slice(1));
+}else{
+  console.log("nope");
+}
 //This combines jQuery with a ES6 arrow function.  Don't worry, we'll get there.
 $(".parentList").on("click", "a", event => {
     event.preventDefault();
@@ -7,5 +16,6 @@ $(".parentList").on("click", "a", event => {
     console.log(href);
     //now we need to load that page into the frame id'ed div.
     $("#frame").load(href);
+    location.hash = href;
 
 })
